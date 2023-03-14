0ac484d96fca2527fb3978451287a37302d44f92
vikas <khanchandani58@gmail.com>
2023-03-14 17:22:07 +0000

    cors

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@ module.exports.socket = (server) => {
   // socket
   const io = require("socket.io")(server, {
     cors: {
-      origin: "https://dev-chat-app.vercel.app",
+      origin: "*",
     },
   });
 
