333a02f9e5c0903428e040ae87bac6f0b23b259a
vikas <khanchandani58@gmail.com>
2023-03-14 17:44:29 +0000

    auth mod

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+// const { check } = require("express-validator");
 const upload = require("../utils/multer");
 const { login, register } = require("../controllers/auth");
 
@@ -8,23 +8,23 @@ const router = express.Router();
 router.post(
   "/register",
   upload.single("image"),
-  [
-    check("userName").not().isEmpty(),
-    check("email").not().isEmpty(),
-    check("password").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
+  // [
+  //   check("userName").not().isEmpty(),
+  //   check("email").not().isEmpty(),
+  //   check("password").not().isEmpty(),
+  //   check("email").normalizeEmail().isEmail(),
+  //   check("password").isLength({ min: 6 }),
+  // ],
   register
 );
 router.post(
   "/login",
-  [
-    check("email").not().isEmpty(),
-    check("password").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
+  // [
+  //   check("email").not().isEmpty(),
+  //   check("password").not().isEmpty(),
+  //   check("email").normalizeEmail().isEmail(),
+  //   check("password").isLength({ min: 6 }),
+  // ],
   login
 );
 
