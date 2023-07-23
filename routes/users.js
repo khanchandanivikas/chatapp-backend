24840b7377b7421a485b6d9050c994082c627974
vikas <khanchandani58@gmail.com>
2023-07-23 12:08:39 +0100

    removed verifyUser

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { getUser } = require("../controllers/user");
-const { verifyUser } = require("../middlewares/verifyToken");
+// const { verifyUser } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
 //get info of a user
-router.get("/:userId", verifyUser, getUser);
+router.get("/:userId", 
+// verifyUser, 
+getUser);
 
 module.exports = router;
