cc82221234d51da6e2e916f6c3f8e8892b38c6bd
vikas <khanchandani58@gmail.com>
2023-07-23 12:13:43 +0100

    removed all verifyUser

--- routes/conversations.js
+++ routes/conversations.js
@@ -4,14 +4,16 @@ const {
   getUserConversation,
   getUsersConversation,
 } = require("../controllers/conversation");
-const { verifyUser } = require("../middlewares/verifyToken");
+// const { verifyUser } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 //new conv
 router.post("/", createConversation);
 
 //get conv of a user
-router.get("/:userId", verifyUser, getUserConversation);
+router.get("/:userId", 
+// verifyUser, 
+getUserConversation);
 
 // get conv includes two userId
 router.get(
--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { createMessage, getUserMessages } = require("../controllers/message");
-const { verifyUser } = require("../middlewares/verifyToken");
+// const { verifyUser } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 //new msg
 router.post("/", createMessage);
 
 //get msgs of a user in a conversation
-router.get("/:conversationId", verifyUser, getUserMessages);
+router.get("/:conversationId", 
+// verifyUser, 
+getUserMessages);
 
 module.exports = router;
