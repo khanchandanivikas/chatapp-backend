82c284bb800b29601354a88836ae19db6049c2f0
vikas <khanchandani58@gmail.com>
2023-08-06 17:38:15 +0100

    get all users filtered with username

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,11 @@ module.exports.getUser = async (req, res, next) => {
 module.exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
-    res.status(200).json(users);
+    const userNames = [];
+    users.forEach((user) => {
+      userNames.push({id: user._id, userName : user.username});
+    })
+    res.status(200).json(userNames);
   } catch (err) {
     return next(createError(500, "There was some unknown error."));
   }
